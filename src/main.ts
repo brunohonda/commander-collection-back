474adfe74c9bee343f19f8617c4590305966553f
Bruno Honda <bruno.honda@live.com>
2022-08-02 08:07:07 -0300

    Added ESLint rule to add semicolon always

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import express, { Router, Request, Response } from 'express'
+import express, { Router, Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
-const route = Router()
+const route = Router();
 
-app.use(express.json())
+app.use(express.json());
 
 route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
-})
+  res.json({ message: 'hello world with Typescript' });
+});
 
-app.use(route)
+app.use(route);
 
-app.listen(3000, () => 'server running on port 3000')
+app.listen(3000, () => 'server running on port 3000');
