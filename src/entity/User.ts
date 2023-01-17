607b0795624fb9ef83954eb534d1c004cf889aab
Bruno Honda <bruno.honda@live.com>
2023-01-17 13:07:09 -0300

    feat(user): define entity attributes

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,12 +1,24 @@
-import { Entity, ObjectIdColumn, ObjectID, Column } from 'typeorm';
+import { Entity, ObjectIdColumn, ObjectID, Column, CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @ObjectIdColumn() _id!: ObjectID;
 
-  @Column() username!: string;
+  @Column()
+  username!: string;
 
-  @Column() password!: string;
+  @Column()
+  password!: string;
 
-  @Column() salt!: string;
+  @Column()
+  salt!: string;
+
+  @CreateDateColumn()
+  createdAt = new Date();
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @VersionColumn()
+  version!: number;
 }
